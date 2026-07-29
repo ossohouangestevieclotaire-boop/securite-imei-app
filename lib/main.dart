3378100d4af835cd08b901749059076206f36e74
ossohouangestevieclotaire-boop <ossohouangestevieclotaire@gmail.com>
2026-07-29 22:03:12 +0000

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sécurité IMEI App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sécurité IMEI'),
+      ),
+      body: const Center(
+        child: Text(
+          'Bienvenue sur votre application !',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
